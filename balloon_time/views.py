e2c340ef85fb078d73d452e6cc4588a55c79b8dd
ladydangerdame <turbofemme@gmail.com>
2018-02-14 02:12:52 -0800

    stubbed starter code

--- balloon_time/views.py
+++ balloon_time/views.py
@@ -7,25 +7,15 @@ from django.urls import reverse_lazy
 
 
 def party_list(request):
-    parties = Party.objects.all()
-    form = PartyForm()
-    return render(request, 'party_list.html', {'parties': parties, 'form':form })
+
 
 def party_detail(request, pk):
-    party = Party.objects.get(pk=pk)
-    return render(request, 'party_detail.html', {'party': party})
+
 
 def post_party(request):
-    form = PartyForm(request.POST)
-    if form.is_valid():
-        party = form.save(commit = False)
-        party.save()
-    return HttpResponseRedirect('/')
+
 
 class PartyDelete(DeleteView):
-    model = Party
-    success_url = reverse_lazy('party_list')
+
 
 class PartyUpdate(UpdateView):
-    model = Party
-    fields = ['title', 'location', 'description']
